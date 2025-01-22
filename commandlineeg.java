323b53af68cee396762e548e67818ae8f42a9e66
aryankhandal <aryankhandal0801@gmail.com>
2025-01-22 15:26:22 +0530

    commandlinneeg

---
+++ commandlineeg.java
@@ -0,0 +1,10 @@
+public class commandlineeg {
+    public static void main(String[] args) {
+        System.out.print(args[0]);
+        System.out.print(" ");
+        System.out.print(args[1]);
+        System.out.print(" ");
+        System.out.print(args[2]);
+        System.out.print(" ");
+    }
+}
